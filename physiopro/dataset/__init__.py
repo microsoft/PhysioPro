a03e39ef676b8278e337dd7732928282da9ad8a0
victorywys <thisiswys@163.com>
2024-07-30 06:54:59 +0000

    fix file name

--- physiopro/dataset/__init__.py
+++ physiopro/dataset/__init__.py
@@ -7,5 +7,5 @@ from .maskts import MaskTSDataset
 from .df import DfDataset
 from .cinc2020 import CinC2020
 from .SEED import SEED
-from .SEED_IV_v1 import SEED_IV
+from .SEED_IV import SEED_IV
 from .tpp import EventDataset
